70e6f8ca538fc338d45d297076d5afd06f0d1f0f
pablomoreira <pablolokekieran@gmail.com>
2023-01-11 21:00:47 -0300

    modified:   main.go

--- main.go
+++ main.go
@@ -1,20 +1,17 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"fmt"
 	"io/ioutil"
 	"log"
+	"os"
+	"os/exec"
 	"strings"
 	"time"
 
-	"fyne.io/fyne/v2"
-	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/data/binding"
-	"fyne.io/fyne/v2/layout"
-	"fyne.io/fyne/v2/widget"
-	ffmpeg "github.com/u2takey/ffmpeg-go"
 	"gopkg.in/yaml.v2"
 )
 
@@ -40,7 +37,7 @@ func main() {
 		fmt.Printf("%s -> %s\n", k, v)
 	}
 	fmt.Scanln()
-	a := app.New()
+	/* := app.New()
 	w := a.NewWindow("Converter")
 
 	content1 := widget.NewLabel("text1")
@@ -67,22 +64,50 @@ func main() {
 	content := container.NewBorder(content1, content3, nil, nil, contentView)
 	w.Resize(fyne.Size{Height: 320, Width: 480})
 	w.SetContent(content)
-
+	*/
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	go timer(str, ctx)
-	singnals := make(chan int)
+	//go timer(str, ctx)
+	signal := make(chan byte)
 
-	go _backW1(ctx, singnals)
+	//go _backW1(ctx, singnals)
 
-	go _backWDir(config["_spath"], config["_dpath"], config["_kw"], config["_args"], data, ctx)
+	//go _backWDir(config["_spath"], config["_dpath"], config["_kw"], config["_args"], data, ctx)
+	go _backWDir(config["_spath"], config["_dpath"], config["_kw"], config["_args"], signal, ctx)
+	//w.ShowAndRun()
 
-	w.ShowAndRun()
+	for _loop := true; _loop == true; {
+		var file *os.File
+		time.Sleep(time.Millisecond * 100)
+		select {
+		case <-signal:
+			log.Print("-")
+			_loop = false
+			file.Close()
+		default:
+			log.Print(".")
+
+			if file == nil {
+				file, err = os.Open("progress.log") // For read access.
+				if err != nil {
+					log.Panic(err)
+				}
+			}
+			fileScanner := bufio.NewScanner(file)
+			fileScanner.Split(bufio.ScanLines)
+			for fileScanner.Scan() {
+				out := strings.Split(fileScanner.Text(), "=")
+				if out[0] == "speed" {
+					fmt.Println(out[1])
+				}
+			}
+		}
+	}
 
 	time.Sleep(time.Second * 2)
-	singnals <- 1
-	cancel()
+	//singnals <- 1
+	//cancel()
 }
 
 func timer(str binding.String, ctx context.Context) {
@@ -98,34 +123,66 @@ func _backW1(ctx context.Context, signal chan int) {
 		select {
 		case <-signal:
 			log.Print("-")
-			i = 1
-		default:
-			//	log.Print(".")
-			time.Sleep(time.Millisecond * 250)
+			for i == 0 {
+				select {
+				case <-signal:
+					log.Print("-")
+					i = 1
+				default:
+					//	log.Print(".")
+					time.Sleep(time.Millisecond * 250)
+				}
+			}
 		}
 	}
 }
 
-func _backWDir(_spath string, _dpath string, _kw string, _args string, _data binding.ExternalStringList, _ctx context.Context) {
+// func _backWDir(_spath string, _dpath string, _kw string, _args string, _data binding.ExternalStringList, _ctx context.Context) {
+// 	time.Sleep(time.Millisecond * 300)
+// 	files, err := ioutil.ReadDir(_spath)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	for _, file := range files {
+// 		if file.IsDir() == false && len(strings.Split(file.Name(), ".")) == 2 {
+// 			if strings.ToLower(strings.Split(file.Name(), ".")[1]) == "mov" {
+// 				ouputfile := strings.ToLower(strings.Split(file.Name(), ".")[0])
+// 				err = ffmpeg.Input(_spath+file.Name()).
+// 					Output(_dpath+ouputfile+".mp4", ffmpeg.KwArgs{"c:v": "h264_amf", "vf": "scale=1024x720", "r": "30"}).
+// 					OverWriteOutput().ErrorToStdOut().Run()
+// 				if err != nil {
+// 					log.Panic("conveter")
+// 				}
+// 				//_data.Append(_dpath + ouputfile + ".mp4")
+// 			}
+// 		}
+// 	}
+// 	time.Sleep(time.Millisecond * 300)
+// }
 
-	time.Sleep(time.Millisecond * 300)
+func _backWDir(_spath string, _dpath string, _kw string, _args string, signal chan byte, _ctx context.Context) {
+	//time.Sleep(time.Millisecond * 300)
 	files, err := ioutil.ReadDir(_spath)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	for _, file := range files {
 		if file.IsDir() == false && len(strings.Split(file.Name(), ".")) == 2 {
 			if strings.ToLower(strings.Split(file.Name(), ".")[1]) == "mov" {
-				ouputfile := strings.ToLower(strings.Split(file.Name(), ".")[0])
-				err = ffmpeg.Input(_spath+file.Name()).
-					Output(_dpath+ouputfile+".mp4", ffmpeg.KwArgs{"c:v": "h264_amf", "vf": "scale=1024x720", "r": "30"}).
-					OverWriteOutput().ErrorToStdOut().Run()
-				if err != nil {
-					log.Panic("conveter")
+				ouputfile := strings.Split(file.Name(), ".")[0] + ".mp4"
+				inputfile := _spath + file.Name()
+				log.Printf("%s -> %s\n", inputfile, ouputfile)
+
+				cmd := exec.Command("ffmpeg", "-y", "-i", inputfile, ouputfile, "-v", "0", "-progress", "progress.log")
+				//cmd.Stdout = os.Stdout
+
+				if err := cmd.Run(); err != nil {
+					fmt.Println("could not run command: ", err)
 				}
-				_data.Append(_dpath + ouputfile + ".mp4")
 			}
 		}
+
 	}
-	time.Sleep(time.Millisecond * 300)
+	signal <- 1
 }
